501146ae0a719a8f9e616880d70cae7ccdd77553
Andy Scout <andyhuzhill@gmail.com>
2012-08-18 14:20:18 +0800

    OK

--- main.c
+++ main.c
@@ -19,10 +19,12 @@
 
 typedef unsigned int uint;
 
-uint Capture = 0;
-uint CapCnt=0;
-uint TA_OverflowCnt=0;
-unsigned long Period=0;
+// 全局变量定义
+uint Capture = 0;  //第一次上升沿捕获值
+uint CapCnt=0;     //捕获计数器
+uint TA_OverflowCnt=0;  //定时器溢出计数
+unsigned long Period=0; //100个周期计数
+uint i,j,k;
 
 void ConfigClocks(void)
 {
@@ -64,7 +66,7 @@ void ConfigPort(void)
 	P1SEL |= BIT4;     //ADC引脚 P1.4
 }
 
-char* ftoa(float f)  //浮点数转换为字符串
+char* ftoa(float f)  //浮点数转换为字符串，保留一位小数
 {
 	static char str[8]={0};
 	long iDat;
@@ -81,17 +83,47 @@ char* ftoa(float f)  //浮点数转换为字符串
 	return str;
 }
 
-void
-main(void)
+void ADC_fuzhi(void)
 {
 	char *str;
-	uint i,j,k;
-	float t,freq;
-
 	uint ADC_value[100];
 	uint ADC_max[10];
 	float volt,average;
 	unsigned long temp;
+
+	ADC_value[i++] = ADC10MEM;           //记录100次ADC转换值，求幅值
+	if(i == 100){
+		i = 0;
+		ADC_max[k] = ADC_value[0];
+
+		for(j = 0;j < 100;j ++){
+			if(ADC_max[k] < ADC_value[j])
+				ADC_max[k] = ADC_value[j];
+		}								//找出100个值中的最大值
+
+		k ++;
+		if(k == 10){
+			temp = 0;
+			for(j = 0; j < 10 ;j ++)
+				temp += ADC_max[j];
+			average = (temp / 10);
+			k = 0;
+			volt =  average / 1023.0 * 1.5;
+			str = ftoa(volt*10);
+			LCD12864_write_string(4,3,str); // 计算十个最大值的平均值作为幅值，并显示
+			LCD12864_write_data('V');
+		}
+	}
+}
+
+
+void
+main(void)
+{
+	char *str;
+
+	float t,freq;
+
 
 	WDTCTL = WDTPW + WDTHOLD;
 	ConfigClocks();
@@ -111,43 +143,19 @@ main(void)
 		ADC10CTL0 |= ADC10SC + ENC;
 		_BIS_SR(CPUOFF+GIE);
 
+		ADC_fuzhi();
 		if (Period != 0) {
 			t = Period / 200.0;
 			freq = 1000000.0 / t / 2;              //将周期转换为频率
-			freq = freq / 1.01;
+			freq = freq / 1.005;
 			str = ftoa(freq*10);
 			LCD12864_write_string(3,2,str);
 			LCD12864_write_data('H');
 			LCD12864_write_data('z');
 			Period = 0;
 		}
-
-		ADC_value[i++] = ADC10MEM;           //记录100次ADC转换值，求幅值
-		if(i == 100){
-			i = 0;
-			ADC_max[k] = ADC_value[0];
-
-			for(j = 0;j < 100;j ++){
-				if(ADC_max[k] < ADC_value[j])
-					ADC_max[k] = ADC_value[j];
-			}								//找出100个值中的最大值
-
-			k ++;
-			if(k == 10){
-				temp = 0;
-				for(j = 0; j < 10 ;j ++)
-					temp += ADC_max[j];
-				average = (temp / 10);
-				k = 0;
-				volt =  average / 1023.0 * 1.5;
-				str = ftoa(volt*10);
-				LCD12864_write_string(4,3,str); // 计算十个最大值的平均值作为幅值，并显示
-				LCD12864_write_data('V');
-			}
-		}
 	}
 }
-
 #pragma vector=TIMER0_A1_VECTOR
 __interrupt void
 TimerA0_ISR(void)
@@ -183,13 +191,6 @@ TimerA0_ISR(void)
 	}
 	_EINT();
 }
-
-//#pragma vector= TIMER1_A1_VECTOR
-//__interrupt void
-//TimerA1_ISR(void)
-//{
-//	ADC10CTL0 = ADC10SC | ENC;
-//}
 
 #pragma vector= ADC10_VECTOR
 __interrupt

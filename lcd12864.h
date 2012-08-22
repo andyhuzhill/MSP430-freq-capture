15070524494e82605244091b431c8081a6a2e7f6
Andy Scout <andyhuzhill@gmail.com>
2012-08-22 18:18:42 +0800

    fix some comment
    Signed-off-by: Andy Scout <andyhuzhill@gmail.com>

--- lcd12864.h
+++ lcd12864.h
@@ -21,74 +21,31 @@
 #include <stdint.h>
 #include "delay.h"
 
-#if 0
 
 /*
- * 4缂��锟介��ゆ�����凤拷杈炬�����烽��ゆ�LCD12864
  *
- * MSP430 濞�拷��CD 锟解��烽��ヤ������ * LCD 4濞达��烽��ゆ���拷�烽��ゆ�婵�拷�烽�锟�D7���锟介��ゆ���拷��0锟斤拷��濞�拷�烽��ゆ���拷U
  *
- * PIN1 --> GND
- * PIN2 --> VCC5V
- * PIN3 --> ���锟介�锟�> GND
- * PIN4 --> RS --> P1.0
- * PIN5 --> R/W --> P1.1
- * PIN6 --> EN --> P1.2
- * PIN7 --> D0 濞�拷�烽�锟�
- * * PIN8 --> D1 濞�拷�烽�锟�
- * * PIN9 --> D2 濞�拷�烽�锟�
- * * PIN10 --> D3 濞�拷�烽�锟�
- * * PIN11 --> D4 --> P2.0
- * PIN12 --> D5 --> P2.1
- * PIN13 --> D6 --> P2.2
- * PIN14 --> D7 --> P2.3
- * PIN15 --> PSB --> 濞�拷�烽�锟�
- * * PIN16 --> NC --> 濞�拷�烽�锟�
- * * PIN17 --> /RESET --> 濞�拷�烽�锟�
- * * PIN18 --> VOUT --> 濞�拷�烽�锟�
- * * PIN19 --> A -->  VCC5V
- * PIN30 --> K --> GND
- *
- */
-
-#define LCD_EN_PORT P1OUT
-#define LCD_EN_DDR P1DIR
-#define LCD_RS_PORT P1OUT
-#define LCD_RS_DDR P1DIR
-#define LCD_RW_PORT P1OUT
-#define LCD_RW_DDR P1DIR
-#define LCD_DATA_PORT P2OUT
-#define LCD_DATA_DDR P2DIR
-#define LCD_EN BIT2
-#define LCD_RS BIT0
-#define LCD_RW BIT1
-#define LCD_DATA BIT3|BIT2|BIT1|BIT0
-
-#endif
-/*
- * 濞�拷锟介��ゆ�锟斤拷�风��э拷����烽��ゆ�LCD12864
- *
- * MSP430 濞�拷��CD 锟解��烽��ヤ������ *
+ *  *
  *
  * PIN1 --> GND
  * PIN2 --> VCC5V
  * PIN3 -->
- * PIN4 --> CS --> P2.0
- * PIN5 --> SID --> P2.1
- * PIN6 --> CLK --> P2.2
- * PIN7 --> D0 濞�拷�烽�锟�
- * * PIN8 --> D1 濞�拷�烽�锟�
- * * PIN9 --> D2 濞�拷�烽�锟�
- * * PIN10 --> D3 濞�拷�烽�锟�
- * * PIN11 --> D4
+ * PIN4 --> CS --> VCC
+ * PIN5 --> SID --> P2.5
+ * PIN6 --> CLK --> P2.4
+ * PIN7 --> D0
+ * PIN8 --> D1
+ * PIN9 --> D2
+ * PIN10 --> D3
+ * PIN11 --> D4
  * PIN12 --> D5
  * PIN13 --> D6
  * PIN14 --> D7
  * PIN15 --> PSB  -->GND
  * PIN16 --> NC
- * PIN17 --> /RESET --> 濞�拷�烽�锟�
- * * PIN18 --> VOUT --> 濞�拷�烽�锟�
- * * PIN19 --> A -->  VCC5V
+ * PIN17 --> /RESET -->
+ * PIN18 --> VOUT -->
+ * PIN19 --> A -->  VCC5V
  * PIN30 --> K --> GND
  *
  *
